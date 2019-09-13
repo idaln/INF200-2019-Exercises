2661c5435a6e4854a5c23fe6ba16d5f0056a0ab7
Ida Lunde Naalsund <idna@nmbu.no>
2019-09-13 10:01:32 +0200

    Completed tasks A to D according to the exercise text

---
+++ src/YOUR_NAME_ex/ex01/tidy_code.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from random import randint as rdint
+
+__author__ = 'Ida Lunde Naalsund'
+__email__ = 'idna@nmbu.no'
+
+
+def make_user_guess():
+    """Makes user guess a number.
+
+    :return: Guess made by user
+    """
+    user_guess = 0
+    while user_guess < 1:
+        user_guess = int(input('Your guess: '))
+    return user_guess
+
+
+def dice_throw():
+    """Simulates two dice throws.
+
+    :return: Random number between 2 and 12
+    """
+    return rdint(1, 6) + rdint(1, 6)
+
+
+def compare_throw_and_guess(throw, guessed_number):
+    """Checks if the user guess and the dice throws
+    have the same value.
+
+    :param throw: Dice throw
+    :param guessed_number: Number guessed by user
+    :return: True if the input elements equal eachother, false otherwise.
+    """
+    return throw == guessed_number
+
+
+if __name__ == '__main__':
+
+    correct_answer = False
+    points = 3
+    dice_sum = dice_throw()
+    while not correct_answer and points > 0:
+        guess = make_user_guess()
+        correct_answer = compare_throw_and_guess(dice_sum, guess)
+        if not correct_answer:
+            print('Wrong, try again!')
+            points -= 1
+
+    if points > 0:
+        print('You won {} points.'.format(points))
+    else:
+        print('You lost. Correct answer: {}.'.format(dice_sum))
