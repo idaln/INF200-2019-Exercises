3753556dc09bea97789e687b5a2836f292e46487
Ida Lunde Naalsund <idna@nmbu.no>
2019-10-21 09:51:41 +0200

    Added main statement in myrand

--- src/ida_lunde_naalsund_ex/ex04/myrand.py
+++ src/ida_lunde_naalsund_ex/ex04/myrand.py
@@ -4,7 +4,7 @@ __author__ = "Ida Lunde Naalsund"
 __email__ = "idna@nmbu.no"
 
 
-class LCGrand:
+class LCGRand:
     def __init__(self, seed):
         self.a = 7**5
         self.m = 2**31-1
@@ -28,3 +28,13 @@ class ListRand:
             random_number = self.list_of_numbers[self.counter]
             self.counter += 1
             return random_number
+
+
+if __name__ == '__main__':
+    lcg_generator = LCGRand(22)
+    print(lcg_generator.rand())
+    print(lcg_generator.rand())
+
+    listrand_generator = ListRand([4, 5, 612, 555, 87, 12, 45])
+    print(listrand_generator.rand())
+    print(listrand_generator.rand())
