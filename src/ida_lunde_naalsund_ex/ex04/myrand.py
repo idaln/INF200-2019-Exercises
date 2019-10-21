ce813761f83b85eeeaf5e3372a036be98eec844a
Ida Lunde Naalsund <idna@nmbu.no>
2019-10-21 09:14:58 +0200

    Created class LCGrand

---
+++ src/ida_lunde_naalsund_ex/ex04/myrand.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Ida Lunde Naalsund"
+__email__ = "idna@nmbu.no"
+
+class LCGrand:
+   def __init__ (self, seed):
+       self.a = 7**5
+       self.m = 2**31-1
+       self.seed = seed
+
+
+   def rand(self):
+       random_number = self.a * self.seed % self.m
+       self.seed = random_number
+       return random_number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ListRand
+    def __init__(self):
+        pass
+
+    def rand(self):
+        pass
