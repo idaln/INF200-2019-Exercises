17c5fc4f87dad5b117955cafa7b58b516d7f4f93
Ida Lunde Naalsund <idna@nmbu.no>
2019-10-21 09:35:43 +0200

    Corrected PEP8 mistakes in myrand and created file test.ex04

--- src/ida_lunde_naalsund_ex/ex04/myrand.py
+++ src/ida_lunde_naalsund_ex/ex04/myrand.py
@@ -3,34 +3,20 @@
 __author__ = "Ida Lunde Naalsund"
 __email__ = "idna@nmbu.no"
 
+
 class LCGrand:
-   def __init__ (self, seed):
-       self.a = 7**5
-       self.m = 2**31-1
-       self.seed = seed
+    def __init__(self, seed):
+        self.a = 7**5
+        self.m = 2**31-1
+        self.seed = seed
+
+    def rand(self):
+        random_number = self.a * self.seed % self.m
+        self.seed = random_number
+        return random_number
 
 
-   def rand(self):
-       random_number = self.a * self.seed % self.m
-       self.seed = random_number
-       return random_number
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class ListRand
+class ListRand:
     def __init__(self):
         pass
 
