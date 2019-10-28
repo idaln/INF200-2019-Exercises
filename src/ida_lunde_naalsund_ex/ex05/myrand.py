595e479715e6e89642fafd5353c7a1a2f772db14
idaln <idna@nmbu.no>
2019-10-28 11:44:13 +0100

    Copied in LCGRand function, added modifications

--- src/ida_lunde_naalsund_ex/ex05/myrand.py
+++ src/ida_lunde_naalsund_ex/ex05/myrand.py
@@ -3,3 +3,126 @@
 __author__ = "Ida Lunde Naalsund"
 __email__ = "idna@nmbu.no"
 
+
+class LCGRand:
+    """Implementation of a linear congruential generator
+    that returns random numbers.
+
+    Attributes
+    ----------
+    slope : int
+        Constant given in ex. text, necessary for generation.
+    congruence_class : int
+        Constant given in ex. text, necessary for generation.
+    """
+    slope = 7**5
+    congruence_class = 2**31 - 1
+
+    def __init__(self, seed):
+        """Initialises the class with given constants a and m.
+
+        Parameters
+        ----------
+        seed : int
+            The seed that the generating is based on.
+        """
+        self._hidden_state = seed
+
+    def rand(self):
+        """Generates a single random number based on a seed given by the user.
+
+        Returns
+        -------
+        random_number : int
+            The generated number.
+        """
+        self._hidden_state *= self.slope
+        self._hidden_state %= self.congruence_class
+
+        return self._hidden_state
+
+    def random_sequence(self, length):
+        pass
+        #return RandIter(self, length)
+
+    def infinite_random_sequence(self):
+        """
+        Generate an infinite sequence of random numbers.
+
+        Yields
+        ------
+        int
+            A random number.
+        """
+        pass
+
+
+class RandIter:
+    def __init__(self, random_number_generator, length):
+        """
+
+        Arguments
+        ---------
+        random_number_generator :
+            A random number generator with a ``rand`` method that
+            takes no arguments and returns a random number.
+        length : int
+            The number of random numbers to generate
+        """
+
+        self.generator = random_number_generator
+        self.length = length
+        self.num_generated_numbers = None
+
+    def __iter__(self):
+        """
+        Initialise the iterator.
+
+        Returns
+        -------
+        self : RandIter
+
+        Raises
+        ------
+        RuntimeError
+            If iter is called twice on the same RandIter object.
+        """
+        if self.num_generated_numbers is not None:
+            raise RuntimeError('iter can only be called once on same '
+                                'RandIter object')
+
+        self.num_generated_numbers = 0
+        return self
+
+    def __next__(self):
+        """
+        Generate the next random number.
+
+        Returns
+        -------
+        int
+            A random number.
+
+        Raises
+        ------
+        RuntimeError
+            If the ``__next__`` method is called before ``__iter__``.
+        StopIteration
+            If ``self.length`` random numbers are generated.
+        """
+        if self.num_generated_numbers is None:
+            raise RuntimeError('__next__ method is called before __iter__')
+
+        if self.num_generated_numbers == self.length:
+            raise StopIteration('All random numbers have been'
+                                'generated')
+
+        self.num_generated_numbers += 1
+
+        return self.generator
+
+
+if __name__ == '__main__':
+    LCG_generator = LCGRand(1)
+    print(LCG_generator.rand())
+    print(LCG_generator.rand())
