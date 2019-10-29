8e58d8f45574e8212ab44f0788ffef517f54d3e1
idaln <idna@nmbu.no>
2019-10-29 16:02:57 +0100

    Created classes BoundedWalker and BoundedSimulation

--- src/ida_lunde_naalsund_ex/ex05/bounded_sim.py
+++ src/ida_lunde_naalsund_ex/ex05/bounded_sim.py
@@ -3,3 +3,55 @@
 __author__ = "Ida Lunde Naalsund"
 __email__ = "idna@nmbu.no"
 
+from .walker_sim import Walker, Simulation
+
+
+class BoundedWalker(Walker):
+    def __init__(self, start, home, left_limit, right_limit):
+        """
+        Initialise the walker
+
+        Arguments
+        ---------
+        start : int
+            The walker's initial position
+        home : int
+            The walk ends when the walker reaches home
+        left_limit : int
+            The left boundary of walker movement
+        right_limit : int
+            The right boundary  of walker movement
+        """
+        self.start = start
+        self.home = home
+        self.left_limit = left_limit
+        self.right_limit = right_limit
+
+        super().__init__(self.start, self.home)
+
+
+class BoundedSimulation(Walker):
+    def __init__(self, start, home, seed, left_limit, right_limit):
+        """
+        Initialise the simulation
+
+        Arguments
+        ---------
+        start : int
+            The walker's initial position
+        home : int
+            The walk ends when the walker reaches home
+        seed : int
+            Random generator seed
+        left_limit : int
+            The left boundary of walker movement
+        right_limit : int
+            The right boundary  of walker movement
+        """
+        self.start = start
+        self.home = home
+        self.seed = seed
+        self.left_limit = left_limit
+        self.right_limit = right_limit
+
+        super().__init__(start, home)
