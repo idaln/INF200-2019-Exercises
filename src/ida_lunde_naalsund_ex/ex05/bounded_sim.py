cb0a6e27c73f407d4fadafb5c3f66ee57ae4ca60
idaln <idna@nmbu.no>
2019-10-30 11:31:51 +0100

    Removed excess seeding, implemented the move method from Walker and fixed main block

--- src/ida_lunde_naalsund_ex/ex05/bounded_sim.py
+++ src/ida_lunde_naalsund_ex/ex05/bounded_sim.py
@@ -3,7 +3,7 @@
 __author__ = "Ida Lunde Naalsund"
 __email__ = "idna@nmbu.no"
 
-from .walker_sim import Walker, Simulation
+from walker_sim import Walker, Simulation
 import random
 
 
@@ -30,24 +30,21 @@ class BoundedWalker(Walker):
 
         super().__init__(self.start, self.home)
 
-    def bounded_move(self):
+    def move(self):
         """
         Makes walker move one step left or right. Whether he walks left or
         right is determined by whether the random number generated is 0 or 1,
         accordingly. Walker is restricted by left and right limit
 
         """
-
         if random.randint(0, 1) == 0:
             if self.position > self.left_limit:
                 self.position -= 1
-                self.steps += 1
+
 
         else:
             if self.position < self.right_limit:
                 self.position += 1
-                self.steps += 1
-
         self.steps += 1
 
 
@@ -71,13 +68,39 @@ class BoundedSimulation(Simulation):
         """
         self.start = start
         self.home = home
-        self.seed = seed
         self.left_limit = left_limit
         self.right_limit = right_limit
 
         super().__init__(start, home, seed)
 
+    def single_walk(self):
+        """
+        Simulate single walk from start to home, returning number of steps.
+
+        Returns
+        -------
+        int
+            The number of steps taken
+        """
+        bounded_walker = BoundedWalker(self.start, self.home, self.left_limit,
+                                       self.right_limit)
+
+        return bounded_walker.walking_process()
 
 if __name__ == '__main__':
-    walk = Walker(0, 5)
-    walk.walking_process()
+
+    num_sim = 20
+
+    start = 0
+    home = 20
+    seed = 1
+    left_limit = [0, -10, -100, -1000, -10000]
+    right_limit = 20
+
+    for limit in left_limit:
+        walk_simulation = BoundedSimulation(start, home, seed,
+                                            limit, right_limit)
+        sim = Simulation(start, home, seed)
+        print(f' Starting position: {start}, home position: {home},'
+              f' seed: {seed} gives length of walks\n'
+              f'{walk_simulation.run_simulation(num_sim)}')
